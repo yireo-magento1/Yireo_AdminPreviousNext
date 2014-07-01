c5dc99d8bcae1004d00d8c342e55221be7ed623a
Jisse Reitsma <info@yireo.com>
2014-07-01 09:00:19 +0200

    Allow for buttons in customer-forms and order-forms also

--- source/app/code/community/Yireo/AdminPreviousNext/Helper/Data.php
+++ source/app/code/community/Yireo/AdminPreviousNext/Helper/Data.php
@@ -13,32 +13,4 @@
  */
 class Yireo_AdminPreviousNext_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public function getPreviousProduct()
-    {
-        $productIds = $this->getProductIds();
-        $currentId = Mage::registry('current_product')->getId();
-        $currentKey = array_search($currentId, $productIds);
-        $previousKey = $currentKey - 1;
-        if($previousKey >= 0 && isset($productIds[$previousKey])) {
-            return $productIds[$previousKey];
-        }
-    }
-
-    public function getNextProduct()
-    {
-        $productIds = $this->getProductIds();
-        $currentId = Mage::registry('current_product')->getId();
-        $currentKey = array_search($currentId, $productIds);
-        $nextKey = $currentKey + 1;
-        if(isset($productIds[$nextKey])) {
-            return $productIds[$nextKey];
-        }
-    }
-
-    public function getProductIds()
-    {
-        // @todo Limit this in some way - start at entity_id X and set page size to 1
-        $productIds = Mage::getModel('catalog/product')->getCollection()->getAllIds();
-        return $productIds;
-    }
 }
